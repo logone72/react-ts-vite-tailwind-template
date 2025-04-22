89f8d8274771612ad0a96cb8323135f4e0f062ee
Roegan Kim 김록원 <roegan.kim@gmail.com>
2025-04-22 11:39:37 +0900

    chore(commitlint): 규칙 추가

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,9 @@
 /**
  * @see https://commitlint.js.org/reference/configuration.html
  */
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-case': [2, 'always', 'lower-case'],
+  },
+};
