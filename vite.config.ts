adbc26855573bb833d957b6a49d9864ec3e7f6dc
Roegan Kim 김록원 <roegan.kim@gmail.com>
2025-04-24 17:50:48 +0900

    chore: client routing - tanstack router 설치 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { checker } from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+import TanstackRouterConfig from './tsr.config.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +16,13 @@ export default defineConfig({
    */
   base: '/react-ts-vite-tailwind-template/',
   plugins: [
+    TanStackRouterVite({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: TanstackRouterConfig.routesDirectory,
+      generatedRouteTree: TanstackRouterConfig.generatedRouteTree,
+      routeFileIgnorePrefix: TanstackRouterConfig.routeFileIgnorePrefix,
+    }),
     react(),
     tsconfigPaths(),
     checker({
